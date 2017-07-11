fc3ec49f909418bd7c572643b5bbd124d7dfbf37
Gabor Szalay <yaastreet@gmail.com>
2017-07-11 18:43:54 +0200

    adding angular router basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { IssueComponent } from './issue/issue.component';
 
+const appRoutes: Routes = [
+  {
+    path: 'edit/:id',
+    component: IssueComponent,
+    data: { id: 'id' }
+  },
+  {
+    path: 'add',
+    component: IssueComponent
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IssueComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
---
+++ src/app/issue/issue.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './issue.component.html',
+  styleUrls: ['./issue.component.css']
+})
+export class IssueComponent {
+  id = 5;
+}
